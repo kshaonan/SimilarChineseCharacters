29131150212ff4d7c94abb4a19f0abcc85d07866
Marvin <wdcpop@gmail.com>
2018-06-04 16:31:45 +0800

    Add config

---
+++ GBDT Algorithm/Config.py
@@ -0,0 +1,7 @@
+
+
+SAMPLE_INPUT_PATH = "C:\\Users\mawentao\PycharmProjects\GBDT\samples\Sample_Input.txt"
+
+DATA_INPUT_PATH = "C:\\Users\mawentao\PycharmProjects\GBDT\samples\Data_Collection.txt"
+
+OUTPUT_PATH = "C:\\Users\mawentao\PycharmProjects\GBDT\samples\Output.txt"
